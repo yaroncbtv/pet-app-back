5cdba9ab331ce9a9eb72837195dcbcb2e46d542a
Yaron Cohen <yaroncbtv@gmail.com>
2020-12-19 21:28:00 +0200

    v3

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var cors = require('cors')
 app.use(cors())
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const { body, validationResult } = require('express-validator');
 
 const url = 'mongodb+srv://yaron:Yaron052@cluster0.3idvo.mongodb.net/YaronDB?retryWrites=true&w=majority';
 
@@ -31,65 +32,118 @@ const url = 'mongodb+srv://yaron:Yaron052@cluster0.3idvo.mongodb.net/YaronDB?ret
 // db.once('open', function() {
 //   console.log('mongoDB Connected!');
 // });
-
+let arrDB =[];
 app.get('/users', (req, res) => {
   MongoClient.connect(url, function(err, client) {
     assert.equal(null, err);
-    
 
-
-    const db = client.db("test");
-    var cursor = db.collection('inventory').find({});
-    function iterateFunc(doc) {
-      // console.log(JSON.stringify(doc, null, 4));
-      let resultFromDB = JSON.stringify(doc, null, 4)
-      resultFromDB = JSON.parse(resultFromDB)
-      console.log(resultFromDB)
-   }
+    const dbo = client.db("test");
+    //var cursor = db.collection('users').find({});
+  //   function iterateFunc(doc) {
+  //     // console.log(JSON.stringify(doc, null, 4));
+  //     let resultFromDB = JSON.stringify(doc, null, 4)
+  //     resultFromDB = JSON.parse(resultFromDB)
+  //   }
    
-   function errorFunc(error) {
-      console.log(error);
-   }
-   
-  cursor.forEach(iterateFunc, errorFunc);
- 
+  //  function errorFunc(error) {
+  //     console.log(error);
+  //  }
+  // cursor.forEach(iterateFunc, errorFunc);
   
-  client.close();
+  // client.close();
+
+  dbo.collection("users").find({}).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    res.send(result)
+    client.close();
+  });
   });
 
-  res.send('Hello World!')
 })
 
 
-app.post('/signup', (req, res) => {
-  MongoClient.connect(url, function(err, client) {
+
+app.post('/signup', [
+  
+  body('email').isEmail(),
+  body('password').isLength({ min: 1 }),
+  body('configPassword').isLength({ min: 1 }),
+  body('phone').isNumeric(),
+  body('configPassword').custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error('Password confirmation does not match password');
+    }
+    return true;
+  })
+  
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+    console.log('work')
+
+    MongoClient.connect(url, function(err, client) {
     assert.equal(null, err);
   
     const db = client.db("test");
     if (err) throw err; 
-    console.log("Record inserted Successfully");
-    db.collection('inventory').insertOne({
+    db.collection('users').insertOne({
       name: req.body.firstName,
       lastName: req.body.lastName,
       email: req.body.email,
       phoneNumber: req.body.phone,
       password: req.body.password
     })
+  
+  
   .then(function(result) {
     // process result
     res.send("succsses")
-    
+    console.log("Record inserted Successfully");
   })
   
     client.close();
   });
 
+});
+
+// app.post('/signup', (req, res) => {
+//   MongoClient.connect(url, function(err, client) {
+//     assert.equal(null, err);
+  
+//     const db = client.db("test");
+//     if (err) throw err; 
+//     // db.collection('inventory').insertOne({
+//     //   name: req.body.firstName,
+//     //   lastName: req.body.lastName,
+//     //   email: req.body.email,
+//     //   phoneNumber: req.body.phone,
+//     //   password: req.body.password
+//     // })
+//     db.createUser(
+//       {
+//         user: "reportsUser",
+//         pwd: passwordPrompt(),  // Or  "<cleartext password>"
+//         roles: [ ]
+//       }
+//    )
+//   .then(function(result) {
+//     // process result
+//     res.send("succsses")
+//     console.log("Record inserted Successfully");
+//   })
+  
+//     client.close();
+//   });
+
   
   
-    //console.log(req.body)
-    //res.send(test)
+//     //console.log(req.body)
+//     //res.send(test)
       
-})
+// })
 
 
 app.listen(port, () => {
